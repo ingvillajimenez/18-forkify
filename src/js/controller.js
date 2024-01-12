117187482221bec33b133548900d9b70cdb10fbd
Juan Villa <ing.villa.jimenez@gmail.com>
2024-01-12 14:23:00 -0600

    Implementing Search Results - Part 2

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,9 +3,15 @@
 import * as model from "./model.js";
 import recipeView from "./views/recipeView.js";
 import searchView from "./views/searchView.js";
+import resultsView from "./views/resultsView.js";
 
 import "core-js/stable"; // Polyfilling other JS features
 import "regenerator-runtime/runtime"; // Polyfilling async functions
+
+if (module.hot) {
+  // hot module reloading from parcel so that state remains
+  module.hot.accept();
+}
 
 // https://forkify-api.herokuapp.com/v2
 
@@ -34,6 +40,9 @@ const controlRecipes = async function () {
 // Implementing Search Results - Part 1
 const controlSearchResults = async function () {
   try {
+    // Rendering spinner
+    resultsView.renderSpinner();
+
     // Get search query
     const query = searchView.getQuery();
     if (!query) return;
@@ -42,7 +51,7 @@ const controlSearchResults = async function () {
     await model.loadSearchResults(query);
 
     // Render results
-    console.log(model.state.search.results);
+    resultsView.render(model.state.search.results);
   } catch (err) {
     console.log(err);
   }
